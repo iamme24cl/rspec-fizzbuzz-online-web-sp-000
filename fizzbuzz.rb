50b3caef4735372b0695d01e161ad7ef19107952
Chandra Lama <iamme24cl@gmail.com>
2020-09-10 02:38:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,12 @@
 
 
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  if number % 15 == 0
-    "FizzBuzz"
+  
   end
 end
